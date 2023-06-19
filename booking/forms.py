e2d0858e1590e5f5add002727de7b7b4e092f028
Alex <alex@Alexs-MacBook-Air.local>
2023-06-19 12:51:44 -0400

    kebabfact

--- booking/forms.py
+++ booking/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-from .models import Booking
+from .models import Booking, Schedule
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
 
 class BookingForm(forms.ModelForm):
     class Meta:
@@ -11,3 +15,33 @@ class BookingForm(forms.ModelForm):
         self.fields['artist_name'].initial = ''
         self.fields['artist_phone_number'].initial = ''
         self.fields['artist_email'].initial = ''
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_time = cleaned_data.get('start_time')
+        end_time = cleaned_data.get('end_time')
+        date = cleaned_data.get('date')
+        studio = cleaned_data.get('studio')
+
+        if start_time and end_time and date and studio:
+            # Check if the selected start time and end time are within the available schedule
+            schedule_qs = Schedule.objects.filter(
+                studio=studio,
+                date=date,
+                start_time__lte=start_time,
+                end_time__gte=end_time
+            )
+            if not schedule_qs.exists():
+                raise ValidationError("Invalid start time or end time.")
+
+            # Check if there are any conflicting bookings for the selected start time and end time
+            # booking_qs = Booking.objects.filter(
+            #     studio=studio,
+            #     date=date,
+            #     start_time__lte=start_time,
+            #     end_time__gte=end_time
+            # )
+            # if booking_qs.exists():
+            #     raise ValidationError("The selected time slot is already booked.")
+        return cleaned_data
+
